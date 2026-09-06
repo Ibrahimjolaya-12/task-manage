4843777b55c035b1de55511299fec3ba948f38d4
Ibrahim JolaYa <ibrahimjolaya2@gmail.com>
2026-09-06 12:03:24 +0500

    done

---
+++ server/src/app.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/authRoutes.js';
+import dns from "dns"
+import workspaceRoutes from './routes/workspaceRoutes.js';
+import projectRoutes from './routes/projectRoutes.js';
+import taskRoutes from './routes/taskRoutes.js';
+import { notFound, errorHandler } from './middleware/error.js';
+
+const app = express();
+
+app.use(express.json({ limit: '1mb' }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true,
+  })
+);
+
+
+if(process.env.ENV !== "production"){
+  dns.setServers(["8.8.8.8","1.1.1.1"])
+}
+
+app.get('/api/health', (_req, res) => res.json({ ok: true }));
+
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/workspaces', workspaceRoutes);
+app.use('/api/v1/projects', projectRoutes);
+app.use('/api/v1/tasks', taskRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+export default app;
